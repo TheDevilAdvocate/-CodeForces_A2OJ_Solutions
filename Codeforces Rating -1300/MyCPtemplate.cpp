05a2e1ac685d3d333eac62af48266c6d206ef457
Akshay Pratap Singh <86912953+TheDevilAdvocate@users.noreply.github.com>
2021-09-30 14:37:17 +0530

    Create MyCPtemplate.cpp

---
+++ Codeforces Rating -1300/MyCPtemplate.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define pb push_back
+#define pf push_front
+using namespace std;
+int main()
+{
+fastread(); 
+int t; 
+cin>>t; 
+while(t--){ 
+    
+
+}
+return 0;
+}
