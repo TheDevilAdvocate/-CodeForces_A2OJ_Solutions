0a1c75925706a59d5ebb10d30b7b8ffc006d3830
Akshay Pratap Singh <86912953+akshayiitism@users.noreply.github.com>
2021-11-26 21:50:52 +0530

    Create Bit++.cpp

---
+++ Codeforces Rating -1300/Bit++.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define loop(i, n) for (ll i = 0; i < (n); i++)
+#define FOR(i, a, b) for (ll i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (ll i = (a); i >= (b); i--)
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+/******READER******/
+template <typename... T>
+void read(T &...args) {
+((cin >> args), ...);
+}
+/******DEBUGGER******/
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__)
+template <typename... Args>
+void logger(string vars, Args &&...values)
+{
+    cout << vars << " = ";
+    string delim = "";
+    (..., (cout << delim << values, delim = ", "));
+}
+/*--------------------------------------------------*/
+int main()
+{
+    fastread();
+    ll t;
+    cin>>t;
+    ll x=0;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        loop(i,s.size()){
+            if(s[i] == '+'){x++;break;}
+            if(s[i] == '-'){x--;break;}
+        }
+    }
+    cout<<x<<endl;
+    return 0;
+}
