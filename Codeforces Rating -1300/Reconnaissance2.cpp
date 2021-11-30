dfa878d8a3a4f5fb7506c914064c427efd652a90
Akshay Pratap Singh <86912953+akshayiitism@users.noreply.github.com>
2021-11-30 14:43:31 +0530

    Create Reconnaissance2.cpp

---
+++ Codeforces Rating -1300/Reconnaissance2.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define loop(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+template <typename... T>
+void read(T &...args) {
+((cin >> args), ...);
+}
+/*****READ CONTAINER****/
+template <typename T>
+void readContainer(T &t) {
+for (auto &e : t) {
+read(e);
+}
+}
+/******PRINT CONTAINER******/
+template <typename... T>
+void write(string delimiter, T &&...args) {
+((cout << args << delimiter), ...);
+}
+template <typename T>
+void writeContainer(string delimiter, T &t) {
+for (const auto &e : t) {
+write(delimiter, e);
+}
+write("\n");
+}
+/******DEBUGGER******/
+#define deb(...) logger(#__VA_ARGS__,__VA_ARGS__)
+template<typename ...Args>
+void logger(string vars, Args&&... values) {
+cout << vars << " = ";
+string delim = "";
+(..., (cout << delim << values, delim= ", "));
+}
+/*--------------------------------------------------*/
+int main()
+{
+fastread();
+    ll n;
+    cin>>n;
+    ll arr[n];
+    loop(i,n){
+        cin>>arr[i];
+    }
+    ll dif=abs(arr[0]-arr[1]);
+    ll a=0,b=1;
+    loop(i,n){
+        ll dif2=abs(arr[i]-arr[(i+1)%n]);
+        if(dif2<dif){
+            a=i;
+
+            b=(i+1)%n;
+            dif=dif2;
+        }
+    }
+    cout<<a+1<< " "  <<b+1<<endl;
+return 0;
+}
