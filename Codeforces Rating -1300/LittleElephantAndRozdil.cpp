091e3e66b0a51894e03fee3192177f79a57defce
Akshay Pratap Singh <86912953+akshayiitism@users.noreply.github.com>
2021-12-01 19:34:49 +0530

    Create LittleElephantAndRozdil.cpp

---
+++ Codeforces Rating -1300/LittleElephantAndRozdil.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define loop(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+template <typename... T>
+void read(T &...args) {
+((cin >> args), ...);
+}
+/*****READ CONTAINER****/
+template <typename T>
+void readContainer(T &t) {
+for (auto &e : t) {
+read(e);
+}
+}
+/******PRINT CONTAINER******/
+template <typename... T>
+void write(string delimiter, T &&...args) {
+((cout << args << delimiter), ...);
+}
+template <typename T>
+void writeContainer(string delimiter, T &t) {
+for (const auto &e : t) {
+write(delimiter, e);
+}
+write("\n");
+}
+/******DEBUGGER******/
+#define deb(...) logger(#__VA_ARGS__,__VA_ARGS__)
+template<typename ...Args>
+void logger(string vars, Args&&... values) {
+cout << vars << " = ";
+string delim = "";
+(..., (cout << delim << values, delim= ", "));
+}
+/*--------------------------------------------------*/
+int main()
+{
+fastread();
+    ll n,flag=0;
+    cin>>n;
+    map<pair<ll,ll>,ll> m;
+    loop(i,n){
+        ll data;
+        cin>>data;
+        pair<ll,ll> temp = {data,i+1};
+        m[temp]++;
+    }
+    ll a = m.begin()->first.first;
+    auto itr = m.begin();
+    itr++;
+    ll b= itr->first.first;
+    if(a == b){
+        cout<<"Still Rozdil\n";
+    }
+    else{
+        cout<<m.begin()->first.second<<endl;
+    }
+    
+return 0;
+}
