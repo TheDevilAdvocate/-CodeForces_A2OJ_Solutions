c2b8aa0a8f30a95ca064855a17223531e1184dfc
Akshay Pratap Singh <86912953+akshayiitism@users.noreply.github.com>
2021-12-06 23:44:32 +0530

    Create Dubstep.cpp

---
+++ Codeforces Rating -1300/Dubstep.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define loop(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+template <typename... T>
+void read(T &...args) {
+((cin >> args), ...);
+}
+/*****READ CONTAINER****/
+template <typename T>
+void readContainer(T &t) {
+for (auto &e : t) {
+read(e);
+}
+}
+/******PRINT CONTAINER******/
+template <typename... T>
+void write(string delimiter, T &&...args) {
+((cout << args << delimiter), ...);
+}
+template <typename T>
+void writeContainer(string delimiter, T &t) {
+for (const auto &e : t) {
+write(delimiter, e);
+}
+write("\n");
+}
+/******DEBUGGER******/
+#define deb(...) logger(#__VA_ARGS__,__VA_ARGS__)
+template<typename ...Args>
+void logger(string vars, Args&&... values) {
+cout << vars << " = ";
+string delim = "";
+(..., (cout << delim << values, delim= ", "));
+}
+/*--------------------------------------------------*/
+vector<string> tokenize(const string str,const regex re){
+	sregex_token_iterator it{ str.begin(),str.end(), re, -1 };
+	vector<string> tokenized{ it, {} };
+	tokenized.erase(remove_if(tokenized.begin(),tokenized.end(),[](string const& s) 
+        {
+						return s.size() == 0;
+                        }),
+		tokenized.end());
+	return tokenized;
+}
+int main()
+{
+	string str;
+    cin>>str;
+	const std::regex re(R"(WUB)");
+	
+	const vector<string> tokenized =
+						tokenize(str, re);
+	
+	for (string token : tokenized)
+		cout << token << " ";
+	return 0;
+}
+
